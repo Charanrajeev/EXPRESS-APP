1f1a38a5bfa9c6c211188c297a33ebffa42f53e3
Charanrajeev <charanforstudy@gmail.com>
2023-04-11 01:37:18 +0530

    newupdate

--- middleware/validateToken.js
+++ middleware/validateToken.js
@@ -4,7 +4,7 @@ const  jwt = require("jsonwebtoken");
 const validateToken = asyncHandler(async (req,res,next)=>{
     let token;
     let authHeader = req.headers.authorization || req.headers.Authorization;
-    if(authHeader&& authHeader.startsWith("Bear")){
+    if(authHeader){
         token = authHeader.split(" ")[1];
         jwt.verify(token,process.env.ACCESS_TOKEN_SECERT,(err,decoded)=>{
             if(err){
